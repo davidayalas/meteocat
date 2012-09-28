520152cbab94cbf96d272b2263fae2438ce9594e
davidayalas <davixyz@gmail.com>
2012-09-28 20:26:38 +0200

    0.1.2

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Catalonia weather stations simple rest service (from meteo.cat)
 ================================================================
 
-I've been trying different aproaches:
+Trying different aproaches to get the data from a free service:
 
-*	Scraperwiki: useful, but free version only allows one scheduled running/day
+*	**Scraperwiki**: useful, but free version only allows one scheduled running/day
 
-*	Google Apps Script: with an standalone service you can develop the business logic and the user interface (in this case a simple rest api)
+*	**Google Apps Script**: with a standalone service you can develop the business logic and the user interface (in this case a simple rest api)
 
 API
 ----
@@ -14,10 +14,10 @@ http://tiny.cc/xema
 
 You can set this params:
 
-*	callback: to request a JSONP
+*	**callback**: to request a JSONP
 
 	http://script.google.com/macros/s/AKfycbz0cyNYyZzQ9thMCAiiTauTtrP58nVymAm8KWx8hhfCZ--yLiA/exec?callback=fn
 
-*	stations: list of values separated by comma (it performs a "contains" and with no accents nor diacritics)
+*	**stations**: list of values separated by comma (it performs a "contains" and with no accents nor diacritics)
 
 	http://script.google.com/macros/s/AKfycbz0cyNYyZzQ9thMCAiiTauTtrP58nVymAm8KWx8hhfCZ--yLiA/exec?callback=fn&stations=alcarras
--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 var cache = CacheService.getPublicCache();
+var db = ScriptDb.getMyDb();
+var ttl = 1800; //same as cron
 
 /**
  * Do replacements to clean text
@@ -16,6 +18,13 @@ function replaces(str){
 
 /**
  * Get content between ">" and next closing "tag" (</tag)
+ *
+ * @param {String} str
+ * @param {Boolean} isInitTagClosed
+ *        if the split has been for a full tag "<tag>" or init "<tag"        
+ * @param {String} tag
+ *        tag to search
+ * @return {String}
  */
 function getTagContent(str,isInitTagClosed,tag){
   return str.slice((isInitTagClosed?0:str.indexOf(">")+1),str.indexOf("</"+tag));      
@@ -86,7 +95,43 @@ function meteo(){
     output.push(e);
   }   
   cache.put("meteodata", JSON.stringify(output));
+  var result = db.query({})
+  while(result.hasNext()) {
+    db.remove(result.next());
+  }    
+  db.save({id:1,timestmp:(new Date()).getTime(),data:output});
   return output;
+}
+
+/**
+ * Get data from cache, db or live
+ *
+ * @return {Object}
+ */
+function getData(){
+  var cached = cache.get("meteodata");
+  var c;
+
+  if(cached!=null){
+    c=cached;
+    Logger.log("from cache");
+  }else{
+    var result = db.query({id:1});
+    if(result.getSize()>0){
+      var current = result.next();
+      if((((new Date()).getTime()-(new Date(current.timestmp)).getTime())/1000)<=ttl){
+        c=current.data;
+        Logger.log("from db");
+      }else{
+        c=meteo();
+        Logger.log("from live");
+      }
+    }else{
+      c=meteo();
+      Logger.log("from live");
+    }
+  }
+  return c;
 }
 
 /**
@@ -97,17 +142,17 @@ function meteo(){
  */
 function doGet(e) {
   var output = ContentService.createTextOutput();
-  var cached = cache.get("meteodata");
-  var c;
-  
-  (cached!=null)?c=cached:c=meteo();
 
+  c = getData();
+  
   var cb = "";
   
+  //callback management
   if(e && e.parameters && e.parameters.callback){
     cb = e.parameters.callback + "(";
   }
 
+  //stations searching
   if(e && e.parameters && e.parameters.stations){
     var st = dropDiacritics(e.parameters.stations.toString().toLowerCase()).split(",");
     c = JSON.parse(c);
